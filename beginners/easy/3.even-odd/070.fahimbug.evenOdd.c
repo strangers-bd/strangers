0c7010711f17c5fd4150b0e6f95b940607256da2
Fahim Mahmud <101657129+fahimbug@users.noreply.github.com>
2022-04-02 21:21:32 +0600

    even or odd

---
+++ beginners/easy/3.even-odd/070.fahimbug.evenOdd.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+    int x ;
+    scanf("%d", &x);
+    if(x % 2 == 0)
+        printf("This is a even number");
+    else
+        printf("This is a odd number");
+    return 0;
+}
