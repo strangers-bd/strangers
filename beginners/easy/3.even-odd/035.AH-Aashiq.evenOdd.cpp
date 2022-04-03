749deba7489e67a6e34502a071d5cbfba5e1e4ad
Ariful haque Asiek <66855223+AH-Aashiq@users.noreply.github.com>
2022-04-03 16:34:06 +0600

    Update 035.AH-Aashiq.evenOdd.cpp

--- beginners/easy/3.even-odd/035.AH-Aashiq.evenOdd.cpp
+++ beginners/easy/3.even-odd/035.AH-Aashiq.evenOdd.cpp
@@ -7,9 +7,9 @@ int main ()
     cin >> number;
 
     if(number % 2 == 0){
-        cout << "This is a even number";
+        cout << "This is a even number\n";
     }else{
-        cout <<"This is a odd number";
+        cout <<"This is a odd number\n";
     }
     return 0;
 }
