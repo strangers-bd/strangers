37dd0f5007830655e381da412e68cb80faf32f5f
Ariful haque Asiek <66855223+AH-Aashiq@users.noreply.github.com>
2022-04-26 16:16:36 +0600

    Efficiency of Carnot;s engine problem solve edited

--- beginners/easy/12.efficiency-of-the-carnot-engine/035.AH-Aashiq.efficiencyOfTheCarnotEngine.cpp
+++ beginners/easy/12.efficiency-of-the-carnot-engine/035.AH-Aashiq.efficiencyOfTheCarnotEngine.cpp
@@ -7,8 +7,8 @@ int main ()
     float n;
     float T1,T2;
 
-    T1 = 373;
-    T2 = 273;
+    T1 = 373; // Celsius to Kelvin for steam point 100 degree = 100C + 273 = 373K
+    T2 = 273; // Celsius to Kelvin for ice point 0degree = 0C + 273 = 273K
 
     n = (1 - (T2 / T1)) * 100;
 
