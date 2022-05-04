d0f9d78fd3aaaeb91153ef6333073bdb2a9483e2
Rishan Khan Real <93808545+rishankhan@users.noreply.github.com>
2022-05-04 19:33:36 +0600

    Update 034.rishankahn.celsius-to-fahrenheit.cpp

--- beginners/easy/5.celsius-to-fahrenheit/034.rishankahn.celsius-to-fahrenheit.cpp
+++ beginners/easy/5.celsius-to-fahrenheit/034.rishankahn.celsius-to-fahrenheit.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 using namespace std;
 int main() {
-    int fahrenheit,celsius;
+    float fahrenheit,celsius;
     cin>>celsius;
     fahrenheit = (celsius * 1.8) + 32;
     cout<<setprecision(2)<<fixed;
