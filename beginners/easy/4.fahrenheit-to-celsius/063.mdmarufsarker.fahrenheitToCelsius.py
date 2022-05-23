cda288892e53609daa0ca13320a02f60e24b980e
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2022-05-23 23:20:42 +0600

    Delete beginners/easy directory

--- beginners/easy/4.fahrenheit-to-celsius/063.mdmarufsarker.fahrenheitToCelsius.py
+++
@@ -1,5 +0,0 @@
-Fahrenheit = float(input("Enter Fahrenheit Temperature: "))
-
-Celsius = (Fahrenheit - 32) / 1.8
-
-print(f"Fahrenheit to Celsius is = {Celsius:.2f}")
--- beginners/easy/5.celsius-to-fahrenheit/063.mdmarufsarker.celsiusToFahrenheit.py
+++
@@ -1,6 +0,0 @@
-
-celsius = float(input("Enter Celsius Temperature: "))
-
-fahrenheit = (celsius * 1.8) + 32
-
-print(f"Celsius to Fahrenheit is = {fahrenheit:.2f}")
