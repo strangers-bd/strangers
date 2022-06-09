b54de1e010cc7315a90dea0da099a0191254a67d
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2022-06-09 20:09:15 +0600

    Create 063.mdmarufsarker.consumption.c

---
+++ beginners/easy/030.consumption/063.mdmarufsarker.consumption.c
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	double X, Y, result;
+	
+	cin >> X;
+	cin >> Y;
+	
+	result = X / Y;
+	
+	printf("%.3lf km/l\n", result);
+	
+	return 0;
+}
