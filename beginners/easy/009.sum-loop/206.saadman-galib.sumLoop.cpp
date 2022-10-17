3d53dfb9eeb1e5f30fc523e7f14f51a536e1c576
Md. Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2022-10-17 14:25:45 +0600

    206.saadman-galib.sumLoop.cpp

---
+++ beginners/easy/009.sum-loop/206.saadman-galib.sumLoop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num, sum;
+    sum = 0;
+    cin >> num;
+    if (num >= 0)
+    {
+        for (int i = 0; i <= num; i++){
+            sum += i;
+        }
+
+        cout << "Total sum = " << sum << endl;
+    }
+
+    else cout << "You have entered a negative number." << endl;
+}
