5fe7f435fc7c4af776a574666b74ea289eeb7a0c
Md. Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2022-10-17 10:36:59 +0600

    206.saadman-galib.even-odd.cpp

---
+++ beginners/easy/003.even-odd/206.saadman-galib.even-odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+    if(num %2 == 0){
+        cout << "This is a even number";
+    }
+    else{
+        cout << "This is a odd number";
+    }
+    return 0;
+}
