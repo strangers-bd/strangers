2e6e9dcb364d3950a49621e632fc8a6f054a28dd
Md. Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2022-10-17 20:11:51 +0600

    206.saadman-galib.efficiencyOfTheCarnotEngine.cpp

---
+++ beginners/easy/012.efficiency-of-the-carnot-engine/206.saadman-galib.efficiencyOfTheCarnotEngine.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    double ef, t1, t2;
+    t1 = 373.0;
+    t2 = 273.0;
+    ef = (1 - (t2 / t1)) * 100;
+    cout << "Efficiency of the Carnot's engine is " << fixed << setprecision(2) << ef << "%";
+}
