be3e380babfe0dc7cd51c58f3f6c483da5efbe81
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2022-12-24 02:00:57 +0600

    206.saadman-galib.basic-calculator.py

---
+++ beginners/easy/033.basic-calculator/206.saadman-galib.basic-calculator.py
@@ -0,0 +1,7 @@
+num1, num2 = map(int, input("Enter two integer number: ").split())
+
+print(f"Summation = {num1 + num2}")
+print(f"Subtraction = {num1 - num2}")
+print(f"Multiplication = {num1 * num2}")
+print(f"Division = {num1 / num2:.2f}")
+print(f"Modulus = {num1 % num2}")
