cb98b1a5bba044caa0c7cf4e812de207e6b67c6f
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2022-12-28 20:25:32 +0600

    206.saadman-galib.valid-triangle(sides).py

--- beginners/easy/031.valid-triangle(angles)/206.saadman-galib.valid-triangle(angles).py
+++ beginners/easy/031.valid-triangle(angles)/206.saadman-galib.valid-triangle(angles).py
@@ -1,7 +1,7 @@
 total = 0
 
 for i in range(1,4):
-    angle = float(input(f"Enter {i} angles of triangle: = "))
+    angle = float(input(f"Enter {i} angle of triangle: = "))
     total += angle
 
 if(total == 180):
