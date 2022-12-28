a4ecd9eb1062a1d4bd73ff999e64cb05f8b8e062
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2022-12-28 16:38:32 +0600

    206.saadman-galib.categorize-person.py

---
+++ beginners/easy/026.categorize-person/206.saadman-galib.categorize-person.py
@@ -0,0 +1,10 @@
+height = float(input("Input the height of the person (in centimetres) : "))
+
+if (height < 150):
+    print("The person is Dwarf.")
+elif(height < 165):
+    print("The person is average heighted.")
+elif(height <= 195):
+    print("The person is taller.")
+else:
+    print("Abnormal Height")
